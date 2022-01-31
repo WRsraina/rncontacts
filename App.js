eabb3411fe4630e2897da0d1247227c731300793
Sumeet Raina <sraina@worldremit.com>
2022-01-31 21:38:06 +0000

    added AppNavContainer contaner component.

--- App.js
+++ App.js
@@ -1,43 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+
+import AppNavContainer from './src/navigation';
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <statusbar barStyle="darkcontent" />
-      <SafeAreaView>
-        <Text>Hello world</Text>
-      </SafeAreaView>
-    </NavigationContainer>
-  );
+  return <AppNavContainer />;
 };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
 
 export default App;
